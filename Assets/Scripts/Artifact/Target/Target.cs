ed50c6e816e97c1d97dc5f2efe2fda907a189d8b
gloddo <makkio94@gmail.com>
2020-02-01 03:19:06 +0100

    added laser beam - added random target

---
+++ Assets/Scripts/Artifact/Target/Target.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    Vector3 scaleChange = new Vector3(-0.003f, -0.003f);
+    void Start()
+    {
+        newTarget();
+    }
+
+    private void newTarget()
+    {
+        transform.localScale = new Vector3(1, 1, 0.5f);
+        transform.RotateAround(Vector3.zero, Vector3.forward, Random.Range(0f, 360f));
+    }
+
+    void Update()
+    {
+        transform.localScale += scaleChange;
+        if(transform.localScale.x <= 0)
+        {
+            newTarget();
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Laser/Laser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+
+    Vector3 scaleChange;
+    void Start()
+    {
+        scaleChange = new Vector3(0.66f, 0);
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (transform.localScale.x <= 2)
+            {
+                transform.localScale += scaleChange;
+            }
+            
+        } 
+        if(Input.GetKeyUp(KeyCode.Space)) {
+            transform.localScale = new Vector3(0.05f, 1, 1);
+        }
+    }
+}
